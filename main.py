37f4e874928ff48c8605f4488d56535d9cc7943d
nanabytz <nandhagopan.s007@gmail.com>
2026-08-07 12:50:47 +0530

    Learned input

--- main.py
+++ main.py
@@ -15,7 +15,7 @@
     #print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-a=int("10")
-b=int("20")
+a=int(input("Enter a number"))
+b=int(input("Enter another number"))
 c=a+b
 print(c)
