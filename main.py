46d5f710100b6fa0769afcfcba63976a77d8b3e8
nanabytz <nandhagopan.s007@gmail.com>
2026-08-09 10:10:00 +0530

    Exercise six done

--- main.py
+++ main.py
@@ -20,15 +20,14 @@ from random import choice
 # See PyCharm help at https://www.jetbrains.com/help/
 
 even=0
-odd=0
-for i in range(1,11):
-    if(i%2==0):
-        even=even+1
-    else:
-        odd=odd+1
 
-print("Even=",even)
-print("Odd=",odd)
+for i in range(1,101):
+    if(i%3==0 and i%5==0):
+        even=even+1
+
+
+print(even)
+
 
 
 
