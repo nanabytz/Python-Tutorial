9de1961dbb391d01b8864e7de2f100ec4b91fb16
nanabytz <nandhagopan.s007@gmail.com>
2026-08-12 20:21:41 +0530

    Exercise two done

--- main.py
+++ main.py
@@ -19,14 +19,15 @@ from random import choice
 
 # See PyCharm help at https://www.jetbrains.com/help/
 
-for i in range(1,3):
-    print("Week:",i)
+for i in range(1,5):
+    print()
     
-    for j in range(1,4):
-        print("Day:",j)
+    for j in range(1,i+1):
+        print(j,end="")
     
 
 
 
 
 
+ 
