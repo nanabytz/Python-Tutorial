817be18dc48f6abf1355c300e7ba531c24e7099c
nanabytz <nandhagopan.s007@gmail.com>
2026-08-15 13:39:31 +0530

    Exercise four done

--- main.py
+++ main.py
@@ -19,10 +19,12 @@ from random import choice
 
 # See PyCharm help at https://www.jetbrains.com/help/
 
-i=10
-while(i<=200):
-   print(i)
-   i=i+10
+i=5
+fact=1
+while(i>0):
+   fact=fact*i
+   i=i-1
+print(fact)
     
 
 
