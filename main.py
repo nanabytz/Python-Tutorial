466cd4c271ddcb0216379364abdc34a3ead89fd0
nanabytz <nandhagopan.s007@gmail.com>
2026-08-15 15:47:16 +0530

    Learning tuple

--- main.py
+++ main.py
@@ -19,13 +19,12 @@ from random import choice
 
 # See PyCharm help at https://www.jetbrains.com/help/
 
-a=[1,2,3,4,5,6]
-b=[11,12,13]
-a.extend(b)
-print(a)    
+a=(1,2,3,4,5,6)
+a[0]=10    #'tuple' object does not support item assignment
+print(a)
 
 
 
 
 
- 
+  
