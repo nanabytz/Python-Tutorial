f585dc272bc7180ef8e3b083c5cc214d20287072
nanabytz <nandhagopan.s007@gmail.com>
2026-08-16 14:11:25 +0530

    Exercise two done

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #print("Hello world")
 # This is a sample Python script.
-import numbers
-from itertools import count
-from random import choice
+#import numbers
+#from itertools import count
+#from random import choice
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -19,12 +19,19 @@ from random import choice
 
 # See PyCharm help at https://www.jetbrains.com/help/
 
-def valueofa():
-    return 10
+s_username="nan"
+s_password="123"
 
-a=valueofa()
+uname = input("Username: ")
+password = input("Password: ")
+
+def validate():
+    if s_username == uname and s_password == password:
+        return "correct"
+    else:
+        return "wrong"
+
+a=validate()
 print(a)
-         
-
 
   
