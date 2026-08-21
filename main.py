3fc6618e5486068366feb4b8f9ec0a8873487815
nanabytz <nandhagopan.s007@gmail.com>
2026-08-21 10:40:56 +0530

    EX 3 Done

--- main.py
+++ main.py
@@ -22,16 +22,20 @@ class grandpa():
     def phone (self) :
         print("Grandpa phone")
 
-class dad():
+class dad(grandpa):
     def money(self):
         print("Dads money")
 
-class son ():
+class son (dad):
     def laptop(self) :
         print("sons laptop")
 
 ram=son()
 ram.laptop()
+ram.money()
+
+d1=dad()
+d1.phone()
 
 
 
